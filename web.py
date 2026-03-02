06cc124faa7a031abff515f5a8fe220eb8f628ea
The Developer Efkan <ei.studios.email@gmail.com>
2026-03-02 22:13:37 +0300

    Delete web.py

--- web.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-
-opener = urllib.request.build_opener()
-opener.addheaders = [('User-Agent', 'Mozilla/5.0')]
-urllib.request.install_opener(opener)
-
-def download(url, file):
-    urllib.request.urlretrieve(url, file)
